5f8fe3df0817d6a2194a696b1b9a6071278643e2
Dominik Szombathy <szmbthy.dmnk@gmail.com>
2026-02-21 19:39:31 +0100

    Solution for Problem 112
    A bit of a brute force but given the small number solution ~1M it is almost instant.

---
+++ python/p_112.py
@@ -0,0 +1,35 @@
+# Project Euler - 112 - Bouncy Numbers
+# Solution by Dominik Szombathy 
+# Date - 2026.02.21
+# Difficulty: 2
+
+# Solution: ###
+
+def is_bouncy(number: int) -> bool:
+    num_str = str(number)
+    decreasing = 0
+    increasing = 0
+    base = num_str[0]
+    for n in num_str[1::]:
+        if int(n) > int(base):
+            increasing += 1
+            base = n
+        elif int(n) < int(base):
+            decreasing += 1
+            base = n
+        
+        
+        if increasing >= 1 and decreasing >= 1:
+            return True
+    
+    return False        
+
+bouncy_number_count = 0
+N = 100
+while bouncy_number_count/N < 0.99:
+    N += 1
+    #if is_bouncy(N):
+    #    print(N)
+    bouncy_number_count += 1 if is_bouncy(N) else 0
+    
+print(N)
